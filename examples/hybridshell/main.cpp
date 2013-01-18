b23f72024c14153d233563f600a359456649aaaf
Milian Wolff <milian.wolff@kdab.com>
2013-01-18 14:45:20 +0100

    Port hybridshell example to newer Qt5
    Change-Id: Ifc6d4cbb647edacfebae83fa84be8f6021483fe7
    Reviewed-by: Pierre Rossi <pierre.rossi@gmail.com>

--- examples/hybridshell/main.cpp
+++ examples/hybridshell/main.cpp
@@ -1,16 +1,18 @@
-#include <QtGui/QApplication>
-#include "qmlapplicationviewer.h"
+#include <QtGui/QGuiApplication>
+
+#include <QtQml>
+
+#include "qtquick2applicationviewer.h"
+
 #include "shell.h"
-#include <qdeclarative.h>
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+    QGuiApplication app(argc, argv);
     qmlRegisterType<Shell>("Qt.labs", 1, 0, "HybridShell");
 
-    QmlApplicationViewer viewer;
-    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
-    viewer.setMainQmlFile(QLatin1String("qml/hybridshell/main.qml"));
+    QtQuick2ApplicationViewer viewer;
+    viewer.setMainQmlFile(QStringLiteral("qml/hybridshell/main.qml"));
     viewer.showExpanded();
 
     return app.exec();
