2a4b4c639bd5f53b068a17cb243cd350dbe875c1
Milian Wolff <milian.wolff@kdab.com>
2013-01-18 16:27:42 +0100

    Refactor the QObject QWebChannel and make the API reusable.
    It might becme a very common use case of the QWebChannel QML plugin.
    Thus it should be as simple as possible for third party consumers to
    setup a QWebChannel for QObject publishing.
    
    The new API basically moves the QtMetaObjectPublisher along with
    the JavaScript marshalling to the qwebchannl/src folder.
    
    The updated qtobject example shows how this new API can be used.
    Furthermore note how it is now trivially possible to register
    multiple objects, which was not easily possible before.
    
    Some notes on the applied refactoring:
    
    - qobject.js contains the JavaScript QObject binding and was
      refactored to support multiple objects.
    - the MetaObjectPublisher contains a new handleRequest function
      which handles the QML-side of the QObject binding. This is
      implemented in QML, while the other book keeping and esp. the
      classInfoForObject is still handled in C++ via the
      QtMetaObjectPublisher class (which is registered as
      MetaObjectPublisherPrivate and used by MetaObjectPublisher)
    
    Change-Id: Id45121bb654447e095bf8a8062d0c8edf9dcb018

--- examples/qtobject/testobject.cpp
+++ examples/qtobject/testobject.cpp
@@ -10,7 +10,7 @@ TestObject::TestObject(QObject *parent) :
 QString TestObject::debugMe(const QString& data)
 {
     qWarning() << data;
-    return "OK";
+    return QString("OK from %1: %2").arg(objectName()).arg(data);
 }
 
 void TestObject::setProp1(const QString& s)
