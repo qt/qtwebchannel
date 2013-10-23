3f8026932650a2dffb4e7ca82bdedc7b0e814e95
Milian Wolff <milian.wolff@kdab.com>
2013-10-23 13:18:13 +0200

    Make it possible to start the examples from a different dir.
    Change-Id: I0a640ee38d642670211b7cae30619fc18307b688
    Reviewed-by: Pierre Rossi <pierre.rossi@gmail.com>

--- examples/hybridshell/main.cpp
+++ examples/hybridshell/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     qmlRegisterType<Shell>("Qt.labs", 1, 0, "HybridShell");
 
     QtQuick2ApplicationViewer viewer;
-    viewer.setMainQmlFile(QStringLiteral("qml/hybridshell/main.qml"));
+    viewer.setMainQmlFile(app.applicationDirPath() + QStringLiteral("/qml/hybridshell/main.qml"));
     viewer.showExpanded();
 
     return app.exec();
