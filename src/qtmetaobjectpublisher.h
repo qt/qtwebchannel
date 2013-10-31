4f0089d3451b99acc130e0c398cbab533959a304
Milian Wolff <milian.wolff@kdab.com>
2013-10-31 16:17:22 +0100

    Simplify code by doing more in QML and less in C++
    Change-Id: I4f4f101b40b3f509371819100ad70d2b2a5731f4

--- src/qtmetaobjectpublisher.h
+++ src/qtmetaobjectpublisher.h
@@ -44,37 +44,15 @@
 
 #include <QObject>
 #include <QVariantMap>
-#include <QPointer>
-#include <QStringList>
 
 class QtMetaObjectPublisher : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(QStringList objectNames READ objectNames)
 public:
     explicit QtMetaObjectPublisher(QObject *parent = 0);
 
-    Q_INVOKABLE QVariantMap classInfoForObject(QObject*) const;
-
-    QStringList objectNames() const { return objects.keys(); }
-
-    Q_INVOKABLE QObject* namedObject(const QString& name) const
-    {
-        if (!objects.contains(name))
-            return 0;
-        return objects[name];
-    }
-
-    Q_INVOKABLE QVariantMap registeredClassInfo() const;
-public slots:
-    void addObject(const QString& name, QObject* object)
-    {
-        objects[name] = object;
-    }
-
-private:
-    QMap<QString, QPointer<QObject> > objects;
-
+    Q_INVOKABLE QVariantMap classInfoForObject(QObject *object) const;
 };
 
 #endif // QTMETAOBJECTPUBLISHER_H
+
