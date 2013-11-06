88ea035c711bb6a1aaddcf44e79a0663bbb54fdc
Milian Wolff <milian.wolff@kdab.com>
2013-11-06 17:18:11 +0100

    Cleanup sources, mostly by removing QtCreator generated bloat.
    This removes a lot of obsolete files and simplifies the build system
    of the examples.
    
    Furthermore, the examples can now be run without running make install
    first. It reuses the same import path as the test does.
    
    Note that the examples are not installable anymore now though. If this
    is required, it can be added again.
    
    Change-Id: Ic7ff80f734b035a03fb1a11a2df492c97298ceff

--- examples/qtobject/main.cpp
+++ examples/qtobject/main.cpp
@@ -1,8 +1,41 @@
+/****************************************************************************
+**
+** Copyright (C) 2013 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author Milian Wolff <milian.wolff@kdab.com>
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of the QWebChannel module on Qt labs.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
 #include <QtGui/QGuiApplication>
 
 #include <QtQml>
+#include <QQuickView>
 
-#include "qtquick2applicationviewer.h"
 #include "testobject.h"
 
 int main(int argc, char *argv[])
@@ -11,9 +44,10 @@ int main(int argc, char *argv[])
     qmlRegisterType<TestObjectFactory>("Qt.labs", 1, 0, "TestObjectFactory");
     qmlRegisterType<TestObject>("Qt.labs", 1, 0, "TestObject");
 
-    QtQuick2ApplicationViewer viewer;
-    viewer.setMainQmlFile(app.applicationDirPath() + QStringLiteral("/qml/qtobject/main.qml"));
-    viewer.showExpanded();
+    QQuickView viewer;
+    viewer.engine()->addImportPath(app.applicationDirPath() + QStringLiteral("/../../src/"));
+    viewer.setSource(QUrl::fromLocalFile(QStringLiteral(SOURCE_DIR) + QStringLiteral("/main.qml")));
+    viewer.show();
 
     return app.exec();
 }
