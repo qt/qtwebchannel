b9246852d8d72f7be9e6e408ada69950974d6e81
Bernd Lamecker <bernd.lamecker@basyskom.com>
2014-07-10 14:59:20 +0200

    Added a nodeJS example/test
    This is a small sample of how to use QWebChannel with NodeJS. The example
    is implemented using NodeJS Mocha testframework.
    See included readme for instructions on how to execute it.
    
    Change-Id: If430703f8c2267df758df9d5978509675c8aea9d

---
+++ tests/nodejs/tests.js
@@ -0,0 +1,64 @@
+/*
+ * These are some very basic tests for the front end API.
+ */
+'use strict';
+var webchannel;
+
+var assert = require('assert');
+var WebSocket = require('faye-websocket');
+
+
+describe('QSON API', function() {
+    //some tests really need some time for communication
+    this.timeout(10000);
+
+    before(function(done) {
+        //var qsonCtor = new require('../qson.js').qson;
+        //qson = new qsonCtor();
+        
+        var socket = new WebSocket.Client('ws://127.0.0.1:1337');
+        var transport = {};
+        
+        socket.on('open', function(event) {
+            console.log("Socket connected");
+            transport.postMessage = function(data) {
+                socket.send(data);
+            }
+            
+            new require('../../src/webchannel/qwebchannel.js').QWebChannel(transport, function(channel) {
+                console.log("Webchannel ready");
+                webchannel = channel;
+                done();
+            });
+        });
+        
+        socket.on('message', function(event) {
+            transport.onmessage(event);
+        });
+        
+        socket.on('error', function (error) {
+            
+        });
+
+        socket.on('close', function () {
+            
+        });
+        
+        
+    });
+
+    beforeEach(function(done) {
+        // prepare client
+        done();
+    });
+
+    afterEach(function() {
+        // cleanup client
+    });
+
+    
+    it('send "NodeJS Test" as a message to the server', function(done) {
+        webchannel.objects.dialog.receiveText("NodeJS Test");
+        done();
+    });
+});
