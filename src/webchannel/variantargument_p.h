92d903d92b430222cd3f89eab08d61d947e5abea
Sergio Martins <iamsergio@gmail.com>
2016-02-06 18:29:14 +0000

    Add the "We mean it" warning to private headers
    Change-Id: I34bcf2e4a2d66c9cb126c2edae79a45064b82a67
    Reviewed-by: Milian Wolff <milian.wolff@kdab.com>

--- src/webchannel/variantargument_p.h
+++ src/webchannel/variantargument_p.h
@@ -34,6 +34,17 @@
 #ifndef VARIANTARGUMENT_H
 #define VARIANTARGUMENT_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QVariant>
 
 QT_BEGIN_NAMESPACE
