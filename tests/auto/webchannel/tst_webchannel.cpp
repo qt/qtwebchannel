qt/qtwebchannel

commit e354bdc5cbe33621def2b200f01d6376291570fc

Fix asynchronous method calls on QObjects in different threads
Use QMetaMethod::invoke without a return for void method calls,
which allows making asynchronous method calls onto QObjects in
different affinities than the QWebChannel that's emitting them.

Also adds a unit test called testAsyncObject that intentionally
places a QObject in a different affinity and then tests calls
into it from the QWebChannel's synchronous publisher.

Task-number: QTBUG-47678
Change-Id: I6c35ee54f764c0fc1b0431fb0774aa7e75039abf
Reviewed-by: Dave Andrews <jetdog330@hotmail.com>
Reviewed-by: Milian Wolff <milian.wolff@kdab.com>

src/webchannel/qmetaobjectpublisher.cpp
@@ -368,22 +368,26 @@ QVariant QMetaObjectPublisher::invokeMethod(QObject *const object, const int met
     for (int i = 0; i < qMin(args.size(), method.parameterCount()); ++i) {
         arguments[i].value = toVariant(args.at(i), method.parameterType(i));
     }
-
     // construct QGenericReturnArgument
     QVariant returnValue;
-    if (method.returnType() != qMetaTypeId<QVariant>() && method.returnType() != qMetaTypeId<void>()) {
-        // Only init variant with return type if its not a variant itself, which would
-        // lead to nested variants which is not what we want.
-        // Also, skip void-return types for obvious reasons (and to prevent a runtime warning inside Qt).
-        returnValue = QVariant(method.returnType(), 0);
-    }
-    QGenericReturnArgument returnArgument(method.typeName(), returnValue.data());
-
-    // now we can call the method
-    method.invoke(object, returnArgument,
+    if (method.returnType() == QMetaType::Void) {
+        // Skip return for void methods (prevents runtime warnings inside Qt), and allows
+        // QMetaMethod to invoke void-returning methods on QObjects in a different thread.
+        method.invoke(object,
                   arguments[0], arguments[1], arguments[2], arguments[3], arguments[4],
                   arguments[5], arguments[6], arguments[7], arguments[8], arguments[9]);
+    } else {
+        QGenericReturnArgument returnArgument(method.typeName(), returnValue.data());
 
+        // Only init variant with return type if its not a variant itself, which would
+        // lead to nested variants which is not what we want.
+        if (method.returnType() != QMetaType::QVariant)
+            returnValue = QVariant(method.returnType(), 0);
+        method.invoke(object, returnArgument,
+                  arguments[0], arguments[1], arguments[2], arguments[3], arguments[4],
+                  arguments[5], arguments[6], arguments[7], arguments[8], arguments[9]);
+    }
+    // now we can call the method
     return returnValue;
 }
 

tests/auto/webchannel/tst_webchannel.cpp
@@ -370,6 +370,12 @@ void TestWebChannel::testInfoForObject()
         }
         {
             QJsonArray method;
+            method.append(QStringLiteral("setProp"));
+            method.append(obj.metaObject()->indexOfMethod("setProp(QString)"));
+            expected.append(method);
+        }
+        {
+            QJsonArray method;
             method.append(QStringLiteral("method1"));
             method.append(obj.metaObject()->indexOfMethod("method1()"));
             expected.append(method);
@@ -476,6 +482,19 @@ void TestWebChannel::testInfoForObject()
                 property.append(signal);
             }
             property.append(QJsonValue::fromVariant(QVariant::fromValue(obj.returnedObject())));
+            expected.append(property);
+        }
+        {
+            QJsonArray property;
+            property.append(obj.metaObject()->indexOfProperty("prop"));
+            property.append(QStringLiteral("prop"));
+            {
+                QJsonArray signal;
+                signal.append(1);
+                signal.append(obj.metaObject()->indexOfMethod("propChanged(QString)"));
+                property.append(signal);
+            }
+            property.append(QJsonValue::fromVariant(QVariant::fromValue(obj.prop())));
             expected.append(property);
         }
         QCOMPARE(info["properties"].toArray(), expected);
@@ -697,6 +716,32 @@ void TestWebChannel::testInfiniteRecursion()
     channel.d_func()->publisher->initializeClient(m_dummyTransport);
 
     QJsonObject objectInfo = channel.d_func()->publisher->wrapResult(QVariant::fromValue(&obj), m_dummyTransport).toObject();
+}
+
+void TestWebChannel::testAsyncObject()
+{
+    QWebChannel channel;
+    channel.connectTo(m_dummyTransport);
+
+    QThread thread;
+    thread.start();
+
+    TestObject obj;
+    obj.moveToThread(&thread);
+
+    QJsonArray args;
+    args.append(QJsonValue("message"));
+
+    int method = obj.metaObject()->indexOfMethod("setProp(QString)");
+    QVERIFY(method != -1);
+
+    QSignalSpy spy(&obj, &TestObject::propChanged);
+    channel.d_func()->publisher->invokeMethod(&obj, method, args);
+    QVERIFY(spy.wait());
+    QCOMPARE(spy.at(0).at(0).toString(), args.at(0).toString());
+
+    thread.quit();
+    thread.wait();
 }
 
 static QHash<QString, QObject*> createObjects(QObject *parent)

tests/auto/webchannel/tst_webchannel.h
@@ -74,6 +74,7 @@ class TestObject : public QObject
     Q_PROPERTY(QString bar READ bar NOTIFY theBarHasChanged)
     Q_PROPERTY(QObject * objectProperty READ objectProperty WRITE setObjectProperty NOTIFY objectPropertyChanged)
     Q_PROPERTY(TestObject * returnedObject READ returnedObject WRITE setReturnedObject NOTIFY returnedObjectChanged)
+    Q_PROPERTY(QString prop READ prop WRITE setProp NOTIFY propChanged)
 
 public:
     explicit TestObject(QObject *parent = 0)
@@ -101,6 +102,11 @@ public:
         return mReturnedObject;
     }
 
+    QString prop() const
+    {
+        return mProp;
+    }
+
     Q_INVOKABLE void method1() {}
 
 protected:
@@ -116,6 +122,7 @@ signals:
     void theBarHasChanged();
     void objectPropertyChanged();
     void returnedObjectChanged();
+    void propChanged(const QString&);
 
 public slots:
     void slot1() {}
@@ -133,6 +140,8 @@ public slots:
         emit objectPropertyChanged();
     }
 
+    void setProp(const QString&prop) {emit propChanged(mProp=prop);}
+
 protected slots:
     void slot3() {}
 
@@ -142,6 +151,7 @@ private slots:
 public:
     QObject *mObjectProperty;
     TestObject *mReturnedObject;
+    QString mProp;
 };
 
 class BenchObject : public QObject
@@ -286,6 +296,7 @@ private slots:
     void testRemoveUnusedTransports();
     void testPassWrappedObjectBack();
     void testInfiniteRecursion();
+    void testAsyncObject();
 
     void benchClassInfo();
     void benchInitializeClients();

commit b5229df6a08a902b11a2fc9529af6385f4d985d5

Do not crash on non-QVariant return types
Patch e354bdc5 introduced a regression that triggers a crash
in Qt 5.9 when returning a type that is not implicitly convertible
to QVariant, such as a QJsonValue, from an invoked method/slot.

This patch fixes this situation and adds proper unit test coverage.

Change-Id: Ib8cb0c96e7496bc8dc9a628245d7a44e4234aff0
Task-number: QTBUG-62045
Reviewed-by: Frederik Gladhorn <frederik.gladhorn@qt.io>
Reviewed-by: Kai Dohmen <psykai1993@googlemail.com>

src/webchannel/qmetaobjectpublisher.cpp
@@ -383,12 +383,12 @@ QVariant QMetaObjectPublisher::invokeMethod(QObject *const object, const int met
                   arguments[0], arguments[1], arguments[2], arguments[3], arguments[4],
                   arguments[5], arguments[6], arguments[7], arguments[8], arguments[9]);
     } else {
-        QGenericReturnArgument returnArgument(method.typeName(), returnValue.data());
-
         // Only init variant with return type if its not a variant itself, which would
         // lead to nested variants which is not what we want.
         if (method.returnType() != QMetaType::QVariant)
             returnValue = QVariant(method.returnType(), 0);
+
+        QGenericReturnArgument returnArgument(method.typeName(), returnValue.data());
         method.invoke(object, returnArgument,
                   arguments[0], arguments[1], arguments[2], arguments[3], arguments[4],
                   arguments[5], arguments[6], arguments[7], arguments[8], arguments[9]);

tests/auto/webchannel/tst_webchannel.cpp
@@ -536,6 +536,10 @@ void TestWebChannel::testInvokeMethodConversion()
         QVERIFY(method != -1);
         channel.d_func()->publisher->invokeMethod(this, method, args);
         QCOMPARE(m_lastInt, args.at(0).toInt());
+        int getterMethod = metaObject()->indexOfMethod("readInt()");
+        QVERIFY(getterMethod != -1);
+        auto retVal = channel.d_func()->publisher->invokeMethod(this, getterMethod, {});
+        QCOMPARE(retVal, args.at(0).toVariant());
     }
     {
         int method = metaObject()->indexOfMethod("setBool(bool)");
@@ -544,24 +548,40 @@ void TestWebChannel::testInvokeMethodConversion()
         args.append(QJsonValue(!m_lastBool));
         channel.d_func()->publisher->invokeMethod(this, method, args);
         QCOMPARE(m_lastBool, args.at(0).toBool());
+        int getterMethod = metaObject()->indexOfMethod("readBool()");
+        QVERIFY(getterMethod != -1);
+        auto retVal = channel.d_func()->publisher->invokeMethod(this, getterMethod, {});
+        QCOMPARE(retVal, args.at(0).toVariant());
     }
     {
         int method = metaObject()->indexOfMethod("setDouble(double)");
         QVERIFY(method != -1);
         channel.d_func()->publisher->invokeMethod(this, method, args);
         QCOMPARE(m_lastDouble, args.at(0).toDouble());
+        int getterMethod = metaObject()->indexOfMethod("readDouble()");
+        QVERIFY(getterMethod != -1);
+        auto retVal = channel.d_func()->publisher->invokeMethod(this, getterMethod, {});
+        QCOMPARE(retVal, args.at(0).toVariant());
     }
     {
         int method = metaObject()->indexOfMethod("setVariant(QVariant)");
         QVERIFY(method != -1);
         channel.d_func()->publisher->invokeMethod(this, method, args);
         QCOMPARE(m_lastVariant, args.at(0).toVariant());
+        int getterMethod = metaObject()->indexOfMethod("readVariant()");
+        QVERIFY(getterMethod != -1);
+        auto retVal = channel.d_func()->publisher->invokeMethod(this, getterMethod, {});
+        QCOMPARE(retVal, args.at(0).toVariant());
     }
     {
         int method = metaObject()->indexOfMethod("setJsonValue(QJsonValue)");
         QVERIFY(method != -1);
         channel.d_func()->publisher->invokeMethod(this, method, args);
         QCOMPARE(m_lastJsonValue, args.at(0));
+        int getterMethod = metaObject()->indexOfMethod("readJsonValue()");
+        QVERIFY(getterMethod != -1);
+        auto retVal = channel.d_func()->publisher->invokeMethod(this, getterMethod, {});
+        QCOMPARE(retVal, args.at(0).toVariant());
     }
     {
         int method = metaObject()->indexOfMethod("setJsonObject(QJsonObject)");
@@ -572,16 +592,24 @@ void TestWebChannel::testInvokeMethodConversion()
         args[0] = object;
         channel.d_func()->publisher->invokeMethod(this, method, args);
         QCOMPARE(m_lastJsonObject, object);
+        int getterMethod = metaObject()->indexOfMethod("readJsonObject()");
+        QVERIFY(getterMethod != -1);
+        auto retVal = channel.d_func()->publisher->invokeMethod(this, getterMethod, {});
+        QCOMPARE(retVal, QVariant::fromValue(object));
     }
     {
-        int method = metaObject()->indexOfMethod("setJsonArray(QJsonArray)");
-        QVERIFY(method != -1);
+        int setterMethod = metaObject()->indexOfMethod("setJsonArray(QJsonArray)");
+        QVERIFY(setterMethod != -1);
         QJsonArray array;
         array << QJsonValue(123);
         array <<  QJsonValue(4.2);
         args[0] = array;
-        channel.d_func()->publisher->invokeMethod(this, method, args);
+        channel.d_func()->publisher->invokeMethod(this, setterMethod, args);
         QCOMPARE(m_lastJsonArray, array);
+        int getterMethod = metaObject()->indexOfMethod("readJsonArray()");
+        QVERIFY(getterMethod != -1);
+        auto retVal = channel.d_func()->publisher->invokeMethod(this, getterMethod, {});
+        QCOMPARE(retVal, QVariant::fromValue(array));
     }
 }
 

tests/auto/webchannel/tst_webchannel.h
@@ -258,20 +258,21 @@ public:
     explicit TestWebChannel(QObject *parent = 0);
     virtual ~TestWebChannel();
 
+public slots:
     int readInt() const;
-    Q_INVOKABLE void setInt(int i);
+    void setInt(int i);
     bool readBool() const;
-    Q_INVOKABLE void setBool(bool b);
+    void setBool(bool b);
     double readDouble() const;
-    Q_INVOKABLE void setDouble(double d);
+    void setDouble(double d);
     QVariant readVariant() const;
-    Q_INVOKABLE void setVariant(const QVariant &v);
+    void setVariant(const QVariant &v);
     QJsonValue readJsonValue() const;
-    Q_INVOKABLE void setJsonValue(const QJsonValue &v);
+    void setJsonValue(const QJsonValue &v);
     QJsonObject readJsonObject() const;
-    Q_INVOKABLE void setJsonObject(const QJsonObject &v);
+    void setJsonObject(const QJsonObject &v);
     QJsonArray readJsonArray() const;
-    Q_INVOKABLE void setJsonArray(const QJsonArray &v);
+    void setJsonArray(const QJsonArray &v);
 
 signals:
     void lastIntChanged();