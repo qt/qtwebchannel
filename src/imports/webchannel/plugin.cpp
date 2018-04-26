5785cb26379de8945f05acad27c2d6619cac1ec8
Simon Hausmann <simon.hausmann@qt.io>
2018-04-26 17:03:00 +0200

    Remove the code to manually initialize resources in static builds
    After commit be9a56e5e3ced5d0d668fa24e4c65ae928f2e25a in qtbase, this is
    not needed anymore. Instead the resource system injects the plugin entry
    point with a reference to all resources.
    
    Change-Id: Iffca1036a6dd2928d4eac89762e102f99ce7dd1a
    Reviewed-by: Milian Wolff <milian.wolff@kdab.com>

--- src/imports/webchannel/plugin.cpp
+++ src/imports/webchannel/plugin.cpp
@@ -43,13 +43,6 @@
 #include <qqmlwebchannel.h>
 #include <qqmlwebchannelattached_p.h>
 
-static void initResources()
-{
-#ifdef QT_STATIC
-    Q_INIT_RESOURCE(qmake_QtWebChannel);
-#endif
-}
-
 QT_BEGIN_NAMESPACE
 
 class QWebChannelPlugin : public QQmlExtensionPlugin
@@ -58,7 +51,7 @@ class QWebChannelPlugin : public QQmlExtensionPlugin
     Q_PLUGIN_METADATA(IID QQmlExtensionInterface_iid)
 
 public:
-    QWebChannelPlugin(QObject *parent = 0) : QQmlExtensionPlugin(parent) { initResources(); }
+    QWebChannelPlugin(QObject *parent = 0) : QQmlExtensionPlugin(parent) { }
     void registerTypes(const char *uri);
 };
 
