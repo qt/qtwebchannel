f5ce8a99c75850dbd6065c86b48656b05d8c6966
Marc Mutz <marc.mutz@qt.io>
2022-11-11 15:53:00 +0100

    Port from container::count() and length() to size() - V5
    This is a the same semantic patch (qt-port-to-std-compatible-api V5
    with config Scope: 'Container') as in dev. I've re-ran it in 6.4 to
    avoid cherry-pick conflicts.
    
    Change-Id: I9621dee5ed328b47e78919a34c307105e4311903
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>

--- tests/auto/webchannel/tst_webchannel.cpp
+++ tests/auto/webchannel/tst_webchannel.cpp
@@ -997,7 +997,7 @@ void TestWebChannel::testWrapObjectWithMultipleTransports()
     pub->wrapResult(QVariant::fromValue(&obj), dummyTransport);
     pub->wrapResult(QVariant::fromValue(&obj), dummyTransport2);
 
-    QCOMPARE(pub->transportedWrappedObjects.count(), 2);
+    QCOMPARE(pub->transportedWrappedObjects.size(), 2);
 }
 
 void TestWebChannel::testJsonToVariant()
@@ -1559,9 +1559,9 @@ void TestWebChannel::qtbug46548_overriddenProperties()
     QSignalSpy spy(&engine, &TestJSEngine::channelSetupReady);
     connect(&engine, &TestJSEngine::channelSetupReady, TestSubclassedFunctor(&engine));
     engine.initWebChannelJS();
-    if (!spy.count())
+    if (!spy.size())
         spy.wait();
-    QCOMPARE(spy.count(), 1);
+    QCOMPARE(spy.size(), 1);
     QJSValue subclassedTestObject = engine.evaluate("channel.objects[\"subclassedTestObject\"]");
     QVERIFY(subclassedTestObject.isObject());
 
